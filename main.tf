da60a41c37e0eff179dd5e3752ac096c6cbbf4a1
Tom Gentry <silvertagback@gmail.com>
2022-11-20 15:01:58 -0700

    security group module

--- main.tf
+++ main.tf
@@ -19,14 +19,28 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "blog_sg" {
+  source              = "terraform-aws-modules/security-group/aws"
+  version             = "4.16.2"
+  name                = "blog_new"
+  
+  vpc_id              = data.aws_vpc.default.id
+  
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
@@ -37,11 +51,11 @@ resource "aws_security_group" "blog" {
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
   
   security_group_id = aws_security_group.blog.id
 }
